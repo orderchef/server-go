022bab596d014d3ff7519d251b779457b9977bea
Zia Ur Rehman <zia.newversion@hotmail.com>
2015-01-17 07:57:29 +0400

    added a script to install dependencies

---
+++ deps.sh
@@ -0,0 +1,16 @@
+# Get project dependencies with `go get`
+
+echo go getting github.com/coopernurse/gorp
+go get github.com/coopernurse/gorp
+
+echo go getting github.com/go-martini/martini
+go get github.com/go-martini/martini
+
+echo go getting github.com/go-sql-driver/mysql
+go get github.com/go-sql-driver/mysql
+
+echo go getting github.com/go-sql-driver/mysql
+go get github.com/martini-contrib/binding
+
+echo go getting github.com/go-sql-driver/mysql
+go get github.com/martini-contrib/render
