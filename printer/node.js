8d3138bb640ff58c64d16e52a628cbcb8f40c860
Matej Kramny <matejkramny@gmail.com>
2015-12-12 22:46:33 +0000

    Fix printer

--- printer/node.js
+++ printer/node.js
@@ -5,7 +5,5 @@ redis.sadd("oc_printers", id);
 redis.subscribe("oc_print." + id);
 
 redis.on("message", function (chan, message) {
-	var obj = JSON.parse(message);
-	console.log(obj);
-	console.log(obj.print);
+	console.log(message);
 });
